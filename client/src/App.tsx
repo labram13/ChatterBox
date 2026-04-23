535eeae1342ededd59cc7581fa1d7ee47b4f08be
labram13 <labram@uw.edu>
2026-04-22 23:06:32 -0700

    created template for protected routes + redirection based on auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,35 @@
 import './App.css'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate, Outlet} from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
+import Dashboard from './components/Dashboard'
 
+function RequireAuth() {
+  const test = true;
+
+  if (test) {
+    return <Outlet />
+  } else {
+    return <Navigate to ='/login'/>
+  }
+}
+
+const isAuthenticated = true;
 
 function App() {
 
   return (
     <div className="container">
       <Routes>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register />} />
+        <Route path='/' element={<Navigate to={isAuthenticated ? '/dashboard' : '/login'} />}/>
+
+        <Route element={<RequireAuth />}>
+          <Route path="/dashboard" element={<Dashboard />}/>
+        </Route>
+
+        <Route path='/login' element={<Navigate to={isAuthenticated ? '/dashboard' : 'login'}/>}/>
+        <Route path='/register' element={<Navigate to={isAuthenticated ? '/dashboard' : 'register'}/>}/>
+
       </Routes>
     </div>
   )
